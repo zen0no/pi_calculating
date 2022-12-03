644d27b2ae658a543c5124aff7fa274e979003cb
zen0no <57628764+zen0no@users.noreply.github.com>
2022-12-03 14:11:32 +0200

    Delete target directory

--- target/debug/deps/pi_approx-5e28fba5640d6b7d.d
+++
@@ -1,5 +0,0 @@
-/home/ondrey/Coding/pi_approx/pi_approx/target/debug/deps/pi_approx-5e28fba5640d6b7d.rmeta: src/main.rs
-
-/home/ondrey/Coding/pi_approx/pi_approx/target/debug/deps/pi_approx-5e28fba5640d6b7d.d: src/main.rs
-
-src/main.rs:
--- target/debug/pi_approx.d
+++
@@ -1,1 +0,0 @@
-/home/ondrey/Coding/pi_approx/pi_approx/target/debug/pi_approx: /home/ondrey/Coding/pi_approx/pi_approx/src/main.rs
